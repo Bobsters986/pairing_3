6083d0b5e98e7af6b18a13675bc75af39bc6b718
Bobby Luly <bobbyluly@gmail.com>
2022-11-03 16:54:17 -0600

    Add new keys

---
+++ hach_practice.rb
@@ -0,0 +1,14 @@
+person = {
+    age: 33,
+    is_male: true,
+    job: "chef",
+    home: "Denver"
+}
+
+p person
+p person[:age]
+
+person[:hobby] = "sports"
+person[:married] = false
+
+p person.keys
